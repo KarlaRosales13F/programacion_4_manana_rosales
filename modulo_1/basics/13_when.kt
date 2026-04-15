12973a2a10b4f77a2213642abe36dba9b0aee1cd
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-15 08:59:18 -0500

    Clase 13-04-2026

--- modulo_1/basics/13_when.kt
+++ modulo_1/basics/13_when.kt
@@ -14,3 +14,21 @@ val especialidad = when(codigo){
 println("Especialidad: $especialidad")
 
 }
+
+//ejemplo
+
+fun main() {
+println("Controles de Flujo When")
+println("Ingrese el numero para seleccionar su tipo de muestra")
+val codigo =readLine()?.toIntOrNull()?:0
+val especialidad = when(codigo){
+    1->"Sangre Venosa | 4h"
+    2 ->"Orina | 2h"
+    3->"Heces | 24h"
+    4->"Hisopado nasofaringeo | 6h"
+    5->"Biopsia | 72h"
+    else ->"Especialidad no registrada en el sistema"
+}
+println("Especialidad: $especialidad")
+
+}
---
+++ modulo_1/basics_mp/12_if_anidado_mp.kt
@@ -0,0 +1,24 @@
+fun main() {
+  println("Controles de Flujo")
+  println("Condicional If - Anidado")
+  println("Tiene antecedentes cardiacos?")
+  val tieneAntecedentes =readLine()?.trim()?.lowercase()=="s"
+  println("Frecuencia Cardiaca (lpm)")
+  val frecuencia =readLine()?.toIntOrNull()?:0
+  if(tieneAntecedentes){
+      println("Paciente con antecedentes cardiacos")
+      if(frecuencia<50) {
+          println("Bradicaria severa")
+      } else{
+          println("Frecuencia Normal")
+      }
+  }else{
+      println("Paciente sin antecedentes cardiacos")
+      if(frecuencia<50 || frecuencia >100){
+          println("Frecuencia fuera del rango normal")
+      }else{
+          println("Frecuencia cardiaca normal")
+      }
+  }
+
+}
