44681c4b3f19830d3e31c4649c1180172cbe63e3
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-27 10:53:18 -0500

    clase 27/04/2026

--- modulo_1/basics/35_producto.kt
+++ modulo_1/basics/35_producto.kt
@@ -60,4 +60,9 @@ fun main() {
     CatalogoProductos.disponibles()
         .map { it.aplicarDescuento(10.0) }
         .forEach { println("  ${it.nombre}: ${"%.2f".format(it.precio)}") }
+
+    println("=== Todos los productos ===")
+    for (p in CatalogoProductos.listar()){
+            println(" ${p.nombre} ${p.precio}")
+        
 }
