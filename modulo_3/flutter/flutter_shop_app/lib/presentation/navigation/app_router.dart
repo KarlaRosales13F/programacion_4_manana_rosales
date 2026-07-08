6d0e334050cbf7f1fca79a4f7bac7a13cb14ff55
KarlaRosales13F <karla.rosalesc06@gmail.com>
2026-07-08 09:29:09 -0500

    SHOPAPP CHECKPOINT 08-ADMIN-CATEGORIES 08-07-2026

--- modulo_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
+++ modulo_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shop_app/presentation/screens/admin/categoriesadminscreen.dart';
 import 'package:flutter_shop_app/presentation/screens/admin/dashboard_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/auth/profile_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/cart/cart_screen.dart';
@@ -124,12 +125,13 @@ final routerProvider = Provider<GoRouter>((ref) {
           child: const DashboardScreen(),
         ),
       ),
+      
       GoRoute(
         path: '/admin/categories',
         builder: (_, state) => AdminShell(
           title: 'Categorías',
           currentRoute: state.matchedLocation,
-          child: const _AdminPlaceholder('Categorías — M8'),
+          child: const CategoriesAdminScreen(),
         ),
       ),
       GoRoute(
